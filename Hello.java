6c0b3caea9fcff259890d6d0326566b8f5059767
Prashant Kumar <22bcs80047@cuchd.in>
2024-11-22 19:41:34 +0000

    hi

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
   public static void main (String[] arg)
   {
-    System.out.println("Hey There! Welcome ");
+    System.out.println("Thanks for visiting my GitHub repository! Here, you’ll discover various projects, tutorials, and examples that I’ve created to share my journey in coding ");
   }
 }
